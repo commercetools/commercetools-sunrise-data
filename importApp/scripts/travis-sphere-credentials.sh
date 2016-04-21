1539fa61c18ae45a54a742c62d23034df77c7a4b
schleichardt <michael.schleichardt@commercetools.de>
2016-04-21 16:48:22 +0200

    update travis build

--- importApp/scripts/travis-sphere-credentials.sh
+++ importApp/scripts/travis-sphere-credentials.sh
@@ -1,7 +1,35 @@
-cat > "importApp/src/test/resources/test.properties" << EOF
-commercetools.projectKey=$PROJECT_KEY
-commercetools.clientId=$CLIENT_ID
-commercetools.clientSecret=$CLIENT_SECRET
-commercetools.authUrl=https://auth.sphere.io
-commercetools.apiUrl=https://api.sphere.io
+cat > "importApp/travis-payload.json" << EOF
+{
+  "commercetools": {
+    "projectKey": "$PROJECT_KEY",
+    "clientId": "$CLIENT_ID",
+    "clientSecret": "$CLIENT_SECRET",
+    "authUrl": "https://auth.sphere.io",
+    "apiUrl": "https://api.sphere.io"
+  },
+  "jobs": [
+    {
+      "name": "productsDeleteJob"
+    },
+    {
+      "name": "categoriesDeleteJob"
+    },
+    {
+      "name": "productTypesDeleteJob"
+    },
+    {
+      "name": "productTypeCreateJob",
+      "resource": "../product-types/product-types.json"
+    },
+    {
+      "name": "categoriesCreateJob",
+      "resource": "../categories/categories.csv"
+    },
+    {
+      "name": "productsCreateJob",
+      "maxProducts": 4000,
+      "resource": "../products/products.csv"
+    }
+  ]
+}
 EOF
