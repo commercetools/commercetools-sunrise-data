4df1d041f4a5ce9568ea39850bb3f6652aee5441
schleichardt <michael.schleichardt@commercetools.de>
2016-04-21 17:45:34 +0200

    use file protocoll for csv files

--- importApp/scripts/travis-sphere-credentials.sh
+++ importApp/scripts/travis-sphere-credentials.sh
@@ -19,16 +19,16 @@ cat > "importApp/travis-payload.json" << EOF
     },
     {
       "name": "productTypeCreateJob",
-      "resource": "$ROOT_PROJ/product-types/product-types.json"
+      "resource": "file://$ROOT_PROJ/product-types/product-types.json"
     },
     {
       "name": "categoriesCreateJob",
-      "resource": "$ROOT_PROJ/categories/categories.csv"
+      "resource": "file://$ROOT_PROJ/categories/categories.csv"
     },
     {
       "name": "productsCreateJob",
       "maxProducts": 4000,
-      "resource": "$ROOT_PROJ/products/products.csv"
+      "resource": "file://$ROOT_PROJ/products/products.csv"
     }
   ]
 }
