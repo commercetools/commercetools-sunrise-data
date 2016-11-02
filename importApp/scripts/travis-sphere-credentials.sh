05f3f97273e43b113c3443191af0f169d85d4b76
schleichardt <michael.schleichardt@commercetools.de>
2016-11-02 10:55:54 +0100

    make tests runnable

--- importApp/scripts/travis-sphere-credentials.sh
+++ importApp/scripts/travis-sphere-credentials.sh
@@ -33,3 +33,11 @@ cat > "importApp/travis-payload.json" << EOF
   ]
 }
 EOF
+
+cat > "/src/test/resources/test.properties" << EOF
+commercetools.projectKey=$PROJECT_KEY
+commercetools.clientId=$CLIENT_ID
+commercetools.clientSecret=$CLIENT_SECRET
+commercetools.authUrl=https://auth.sphere.io
+commercetools.apiUrl=https://api.sphere.io
+EOF
--- importApp/src/test/java/com/commercetools/dataimport/categories/JoyrideIntegrationTest.java
+++ importApp/src/test/java/com/commercetools/dataimport/categories/JoyrideIntegrationTest.java
@@ -1,36 +1,26 @@
 package com.commercetools.dataimport.categories;
 
-import com.commercetools.dataimport.commercetools.CommercetoolsConfiguration;
+import com.commercetools.CommercetoolsTestConfiguration;
 import com.commercetools.dataimport.joyrideavailability.InventoryEntryCreationJobConfiguration;
-import io.sphere.sdk.categories.Category;
-import io.sphere.sdk.categories.CategoryDraftBuilder;
-import io.sphere.sdk.categories.commands.CategoryCreateCommand;
-import io.sphere.sdk.categories.queries.CategoryQuery;
 import io.sphere.sdk.client.BlockingSphereClient;
 import io.sphere.sdk.inventory.InventoryEntry;
 import io.sphere.sdk.inventory.InventoryEntryDraft;
 import io.sphere.sdk.inventory.commands.InventoryEntryCreateCommand;
 import io.sphere.sdk.models.LocalizedString;
 import io.sphere.sdk.products.Product;
-import io.sphere.sdk.products.ProductDraft;
 import io.sphere.sdk.products.ProductDraftBuilder;
 import io.sphere.sdk.products.ProductVariantDraftBuilder;
 import io.sphere.sdk.products.commands.ProductCreateCommand;
-import io.sphere.sdk.products.search.ProductProjectionSearch;
 import io.sphere.sdk.producttypes.ProductType;
 import io.sphere.sdk.producttypes.ProductTypeDraft;
 import io.sphere.sdk.producttypes.commands.ProductTypeCreateCommand;
-import io.sphere.sdk.queries.PagedQueryResult;
 import org.apache.commons.lang3.RandomStringUtils;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.batch.core.JobExecution;
-import org.springframework.batch.test.JobLauncherTestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -38,18 +28,17 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import java.util.Collections;
 import java.util.Locale;
 import java.util.Optional;
-import java.util.Random;
 
-import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {CommercetoolsConfiguration.class})
+@ContextConfiguration(classes = {CommercetoolsTestConfiguration.class})
 @EnableAutoConfiguration
 @Configuration
 @TestPropertySource("classpath:/test.properties")
 public class JoyrideIntegrationTest {
     @Autowired
+    @Qualifier("test")
     private BlockingSphereClient sphereClient;
 
 
