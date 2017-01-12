3f6aef69f3400b36cca1f94a1570f3882f0b5ad7
cc.salazar <cristian.salazar@commercetools.de>
2017-01-12 16:49:48 +0100

    Fix channels Import Test

--- importApp/src/test/java/com/commercetools/dataimport/joyrideavailability/ChannelsImportJobIntegrationTest.java
+++ importApp/src/test/java/com/commercetools/dataimport/joyrideavailability/ChannelsImportJobIntegrationTest.java
@@ -2,9 +2,12 @@ package com.commercetools.dataimport.joyrideavailability;
 
 import com.commercetools.CommercetoolsTestConfiguration;
 import com.commercetools.dataimport.categories.TestConfiguration;
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.ObjectReader;
+import com.fasterxml.jackson.databind.type.TypeFactory;
 import io.sphere.sdk.channels.Channel;
 import io.sphere.sdk.channels.queries.ChannelQuery;
-import io.sphere.sdk.queries.PagedQueryResult;
+import io.sphere.sdk.json.SphereJsonUtils;
 import io.sphere.sdk.types.Type;
 import io.sphere.sdk.types.queries.TypeQuery;
 import net.jcip.annotations.NotThreadSafe;
@@ -16,14 +19,19 @@ import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.test.JobLauncherTestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.core.io.Resource;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static com.commercetools.dataimport.joyrideavailability.JoyrideAvailabilityUtils.*;
@@ -43,34 +51,47 @@ public class ChannelsImportJobIntegrationTest extends JoyrideAvailabilityIntegra
     @Autowired
     private Environment env;
 
+    @Autowired
+    private ApplicationContext appContext;
+
     @Test
     public void channelImportStepTest() throws Exception {
         final Map<String, JobParameter> jobParametersMap = new HashMap<>();
-        jobParametersMap.put("typesResource", new JobParameter("file://" + new File(".", "../joyride/types.json").getAbsolutePath()));
-        jobParametersMap.put("channelsResource", new JobParameter("file://" + new File(".", "../joyride/channels.json").getAbsolutePath()));
+        final String channelsResourcePath = "file://" + new File(".", "../joyride/channels.json").getAbsolutePath();
+        final String typesResourcePath = "file://" + new File(".", "../joyride/types.json").getAbsolutePath();
+        jobParametersMap.put("typesResource", new JobParameter(typesResourcePath));
+        jobParametersMap.put("channelsResource", new JobParameter(channelsResourcePath));
         addCommercetoolsCredentialValues(env, jobParametersMap);
         final JobParameters jobParameters = new JobParameters(jobParametersMap);
         final ExitStatus status = jobLauncherTestUtils.launchJob(jobParameters).getExitStatus();
         assertThat(status).isEqualTo(ExitStatus.COMPLETED);
-        checkChannels();
-        checkTypes();
+        checkChannels(channelsResourcePath);
+        checkTypes(typesResourcePath);
     }
 
-    private void checkChannels() {
+    private void checkChannels(final String channelsResourcePath) throws IOException {
+        Resource resource = appContext.getResource(channelsResourcePath);
+        final List<Channel> channelsFromJsonFile = fetchListFromJsonResource(resource, Channel.class);
         final ChannelQuery channelQuery = ChannelQuery.of();
-        final PagedQueryResult<Channel> channelPagedQueryResult = sphereClient.executeBlocking(channelQuery);
-        assertThat(channelPagedQueryResult.getTotal()).isEqualTo(16);
+        final List<Channel> channelList = sphereClient.executeBlocking(channelQuery).getResults();
+        assertThat(channelList).hasSize(channelsFromJsonFile.size());
         deleteChannels(sphereClient);
     }
 
-    private void checkTypes() {
-        final TypeQuery typeQuery = TypeQuery.of().withSort(m -> m.key().sort().asc());
-        final PagedQueryResult<Type> typePagedQueryResult = sphereClient.executeBlocking(typeQuery);
-        assertThat(typePagedQueryResult.getTotal()).isEqualTo(2);
-        final Type type1 = typePagedQueryResult.getResults().get(0);
-        assertThat(type1.getKey()).isEqualTo("physicalStore");
-        final Type type2 = typePagedQueryResult.getResults().get(1);
-        assertThat(type2.getKey()).isEqualTo("reservationOrder");
+    private void checkTypes(final String typesResourcePath) throws IOException {
+        Resource resource = appContext.getResource(typesResourcePath);
+        final List<Type> typesFromJsonFile = fetchListFromJsonResource(resource, Type.class);
+        final TypeQuery typeQuery = TypeQuery.of();
+        final List<Type> typeList = sphereClient.executeBlocking(typeQuery).getResults();
+        assertThat(typeList).hasSize(typesFromJsonFile.size());
         deleteTypes(sphereClient);
+    }
+
+    private <T> List<T> fetchListFromJsonResource(final Resource resource, final Class<T> clazz) throws IOException {
+        final TypeFactory typeFactory = TypeFactory.defaultInstance();
+        final JavaType javaType = typeFactory.constructCollectionType(List.class, clazz);
+        final ObjectReader reader = SphereJsonUtils.newObjectMapper().readerFor(javaType);
+        final InputStream inputStream = resource.getInputStream();
+        return reader.readValue(inputStream);
     }
 }
