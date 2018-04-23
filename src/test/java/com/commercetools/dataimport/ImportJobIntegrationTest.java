2c701619afa332d181965e6749e8dcc92017eb24
sshibani <siawash.shibani@commercetools.com>
2018-04-23 13:55:03 +0200

    enabled unittest asserts for products, orders and inventory

--- src/test/java/com/commercetools/dataimport/ImportJobIntegrationTest.java
+++ src/test/java/com/commercetools/dataimport/ImportJobIntegrationTest.java
@@ -16,6 +16,9 @@ public class ImportJobIntegrationTest extends AbstractIntegrationTest {
         assertThat(fetchTotalTypes("channel")).as("Channel types are imported").isEqualTo(1);
         assertThat(fetchTotalOrders()).as("Orders are imported").isEqualTo(2);
         assertThat(fetchTotalProductTypes()).as("Product types are imported").isEqualTo(1);
+        assertThat(fetchTotalProducts()).as("Products are imported").isEqualTo(2);
+        assertThat(fetchTotalChannels()).as("Channels are imported").isEqualTo(18);
+        assertThat(fetchTotalInventory()).as("Inventory is imported").isEqualTo(4);
 
         final Project project = sphereClient.executeBlocking(ProjectGet.of());
         assertThat(project.getCountries()).hasSize(2);
